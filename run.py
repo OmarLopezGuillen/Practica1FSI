cb879bc445cad56de0c41382b692f70a4cf81524
OmarLopezGuillen <omar.estudio36@gmail.com>
2022-11-14 19:17:06 +0000

    pruebas hechas

--- run.py
+++ run.py
@@ -8,12 +8,48 @@ ab = search.GPSProblem('A', 'B'
 ab2 = search.GPSProblem('A', 'B'
                        , search.romania)
 
+ab3 = search.GPSProblem('O', 'U'
+                       , search.romania)
+                       
+ab4 = search.GPSProblem('L', 'N'
+                       , search.romania)
+
+ab5 = search.GPSProblem('S', 'E'
+                       , search.romania)
+
+
 print(search.breadth_first_graph_search(ab).path())
+
+print("#####################")
+print("\n")
+
+
 print(search.depth_first_graph_search(ab).path())
+
+print("#####################")
+print("\n")
 
 print(search.branch_and_bounds_graph_search(ab).path())
 
+print("#####################")
+print("\n")
+
 print(search.metodo_subestimacion(ab2).path())
+
+print("#####################")
+print("\n")
+
+print(search.metodo_subestimacion(ab3).path())
+
+print("#####################")
+print("\n")
+
+print(search.metodo_subestimacion(ab4).path())
+
+print("#####################")
+print("\n")
+
+print(search.metodo_subestimacion(ab5).path())
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
